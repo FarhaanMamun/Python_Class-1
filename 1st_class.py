435a2d4f4f17d9389c6edf7fa79e630e5e19e9a1
unknown <farhaanmamu0087@gmail.com>
2022-05-21 16:27:38 +0600

    First Class

---
+++ 1st_class.py
@@ -0,0 +1,34 @@
+# Arithmetic Operation taken by value / variable with help of input
+value = input(19+19)
+print(value)
+value = input(19-19)
+print(value)
+value = input(19*19)
+print(value)
+value = input(19/19)
+print(value)
+
+# String value
+string = input("Farhaan Mamun")
+print(string)
+
+print("Hello World!")
+print(5)
+print(5.0)
+x = 5
+print(x)
+
+y = "Hello World"
+print(y)
+
+#This is Comment
+"""this
+is 
+multiple line comment"""
+
+x = 6
+y = 10
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
